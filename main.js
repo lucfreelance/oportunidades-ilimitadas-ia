46b8d9a87da769d00947d52b13a302e56be54140
sslluuccaa <soelucas3@gmail.com>
2023-03-06 16:40:47 -0500

    Integrando elemento de animacion CSS en header...

--- main.js
+++ main.js
@@ -10,8 +10,8 @@ window.onload = function() {
 
   // Working with credits
 
-var cpCreditsUrl = "https://codepen.io/PickJBennett/details/BdbrMW";
-var cpCreditsTitle = "🌈 Sexy Animated Rainbow Waves Header";
-var cpCreditsTwitter = document.getElementById("js-tweet-this");
-cpCreditsTwitter.href = "https://twitter.com/intent/tweet?text=" + encodeURI(cpCreditsTitle) + "&url=" + encodeURI(cpCreditsUrl) + "&via=PickJBennett&related=PickJBennett,CiaoFileManager";
-cpCreditsTwitter.innerHTML += "Tweet This Pen";
+// var cpCreditsUrl = "https://codepen.io/Pickhelloennett/details/BdbrMW";
+// var cpCreditsTitle = "🌈 Sexy Animated Rainbow Waves Header";
+// var cpCreditsTwitter = document.getElementById("js-tweet-this");
+// cpCreditsTwitter.href = "https://twitter.com/intent/tweet?text=" + encodeURI(cpCreditsTitle) + "&url=" + encodeURI(cpCreditsUrl) + "&via=PickJBennett&related=PickJBennett,CiaoFileManager";
+// cpCreditsTwitter.innerHTML += "Tweet This Pen";
